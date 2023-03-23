2396e6e2f7404d96f8a7b6a58ca4d8256d99a0f4
fis48 <info@fidelsilva.com>
2023-03-23 11:52:04 -0500

    - CRA typescript added. - React Bootstrap added. - React Icons added.

---
+++ ffapp/src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { DiReact } from "react-icons/di";
+
+function App() {
+  return (
+    <>
+      <h1>
+        <DiReact />
+        ffApp
+      </h1>
+    </>
+  );
+}
+
+export default App;
