1fc0db6aea6fecb0c4e3f9762936421d83080f93
fis48 <info@fidelsilva.com>
2023-03-25 10:16:27 -0500

    reset for clean branches start

--- ffapp/src/App.tsx
+++ ffapp/src/App.tsx
@@ -1,15 +1,9 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import { DiReact } from "react-icons/di";
-
 function App() {
   return (
     <>
-      <h1>
-        <DiReact />
-        ffApp
-      </h1>
     </>
   );
 }
